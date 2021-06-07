ec9247d5a175b14381d110524e0aa0e125fc8cdd
Fanchi852 <fjmo852@gmail.com>
2021-06-07 10:44:38 +0200

    plantilla de proyecto springboot

---
+++ src/main/java/com/example/BusTransport3/BusTransport3Application.java
@@ -0,0 +1,13 @@
+package com.example.BusTransport3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTransport3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTransport3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BusTransport3/BusTransport3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusTransport3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTransport3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
